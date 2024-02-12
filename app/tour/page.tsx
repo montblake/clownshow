3ccada778101a4b055395a89371b60a3e6408dac
Blake Montgomery <blakemontgomery312@gmail.com>
2024-02-12 10:38:58 -0600

    Add Second Gen Tour Page placeholder

--- app/tour/page.tsx
+++ app/tour/page.tsx
@@ -1,13 +1,4 @@
 // // app/dashboard/page.tsx
-// import { Suspense } from 'react';
-// import {
-//   UnbookedPresentersSkeleton,
-//   MobyTourSkeleton,
-//   CardsSkeleton,
-// } from '@/components/skeletons';
-// import CardsWrapper from '@/components/tour/cards';
-// import MobyDickPerformances from '@/components/tour/mobydick-tour';
-// import UnbookedPresenters from '@/components/tour/unbooked-presenters';
 
 export default async function Page() {
   return (
@@ -15,19 +6,10 @@ export default async function Page() {
       <h1 className="mb-4 text-xl md:text-2xl">Touring</h1>
       <div className="mt-4">
         <h2>Tour Data, at a glance</h2>
-        {/* <Suspense fallback={CardsSkeleton}>
-          <CardsWrapper />
-        </Suspense> */}
       </div>
 
       <div className="mt-6 grid w-full grid-cols-1 gap-6 md:grid-cols-4">
         <h2>Important Data, topline details</h2>
-        {/* <Suspense fallback={MobyTourSkeleton}>
-          <MobyDickPerformances />
-        </Suspense> */}
-        {/* <Suspense fallback={UnbookedPresentersSkeleton}>
-          <UnbookedPresenters />
-        </Suspense> */}
       </div>
     </main>
   );
