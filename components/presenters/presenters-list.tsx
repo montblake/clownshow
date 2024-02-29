f40d5d873f2c55be7b71edcfd5c1924bd8560a3f
Blake Montgomery <blakemontgomery312@gmail.com>
2024-02-29 02:13:30 -0600

    Add shadcn-ui to presenters-list

--- components/presenters/presenters-list.tsx
+++ components/presenters/presenters-list.tsx
@@ -1,6 +1,15 @@
 import { fetchFilteredPresenters } from '@/lib/data';
+import { PresenterFields } from '@/lib/definitions';
 import { EditPresenter } from '@/components/presenters/buttons';
 import ConfirmDeleteModal from '@/components/confirm-delete-modal';
+import {
+  Card,
+  CardContent,
+  CardDescription,  
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default async function PresentersList({
   query,
@@ -9,31 +18,29 @@ export default async function PresentersList({
   query: string;
   currentPage: number;
 }) {
-  const presenters = await fetchFilteredPresenters(query, currentPage);
+  const presenters: PresenterFields[] = await fetchFilteredPresenters(query, currentPage);
+
   return (
     <ul className="grid w-full grid-cols-1 gap-6 sm:grid-cols-2 xl:grid-cols-3">
       {presenters?.map((p) => (
-        <li
-          key={p.id}
-          className="flex flex-col items-start justify-start rounded-lg bg-slate-50 p-4 text-xl text-slate-800 drop-shadow"
-        >
-          <div className="mb-2 flex">
-            <EditPresenter id={p.id} />
-            <ConfirmDeleteModal resourceType="presenter" resourceId={p.id} />
-          </div>
-          <div className="mb-2">
-            <h3 className="text-2xl font-bold">{p.name}</h3>
-            <p>{p.location}</p>
-          </div>
-          <div className="mb-2">
-            <h4 className="text-sm italic">Contact</h4>
+        <Card key={p.id}>
+          <CardHeader>
+            <CardTitle>{p.name}</CardTitle>
+            <CardDescription>{p.location}</CardDescription>
+          </CardHeader>
+          <CardContent>
+          <h4 className="text-sm italic">Contact</h4>
             <p className="mb-2">{p.contact_name}</p>
             <h4 className="text-sm italic">Email</h4>
             <p className="mb-2">{p.contact_email}</p>
             <h4 className="text-sm italic">Phone</h4>
             <p>{p.contact_phone}</p>
-          </div>
-        </li>
+          </CardContent>
+          <CardFooter>
+            <EditPresenter id={p.id} />
+            <ConfirmDeleteModal resourceType="presenter" resourceId={p.id} />
+          </CardFooter>
+        </Card>
       ))}
     </ul>
   );
