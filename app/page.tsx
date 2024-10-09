f29eba59eeb1bcbc04244596a2e0e891ec99e635
Blake Montgomery <blakemontgomery312@gmail.com>
2024-10-09 11:00:57 -0500

    Header Fixed Position

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="min-h-screen w-screen mx-auto bg-white flex flex-col justify-start items-center">
-      <header className="flex flex-col items-center justify-center w-full bg-white px-12 py-12 sm:px-16 md:px-20 lg:px-24">
+      <header className="flex flex-col items-center justify-center w-full bg-white px-12 py-12 sm:px-16 md:px-20 lg:px-24 fixed top-0">
         <h1 className="font-heading font-bold text-base sm:text-lg md:text-xl lg:text-2xl tracking-tighter text-neutral-800  drop-shadow-2xl max-w-[600px]">
           <span className="text-red-600 text-3xl">clownshow</span> is a creative
           production company specializing in the development of original live
           theater, immersive events, and boundary-pushing experiences.
         </h1>
       </header>
-      <section className="w-full bg-black h-3/4">
+      <section className="w-full bg-black h-3/4 mt-40 sm:mt-44 md:mt-48 lg:mt-52">
         <div className="px-12 py-12 sm:px-16 md:px-20 lg:px-24">
           <a href="https://www.begrudginglydickens.com" target="_blank">
             <div className="flex flex-col items-center justify-center gap-4 mb-24 w-full bg-red-600 p-8 rounded-lg hover:border-white border-4 border-red-600 max-w-[600px] mx-auto">
@@ -30,6 +30,12 @@ export default function Home() {
                 height={800}
                 className="rounded-lg border border-white"
               />
+              {/* <hr className="h-[1px] w-full my-0 bg-white border-0" /> */}
+
+              <p className="text-white text-sm sm:text-sm md:text-base lg:text-lg mt-2">
+                click to learn more
+              </p>
+              {/* <hr className="h-[1px] w-full my-0 bg-white border-0 mb-4" /> */}
             </div>
           </a>
         </div>
