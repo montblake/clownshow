5ea8774d64e4ef807304e3f65af720538ad39565
Blake Montgomery <blakemontgomery312@gmail.com>
2024-12-16 13:07:21 -0600

    Add contact email

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,23 @@ export default function Home() {
           production company specializing in the development of original live
           theater, immersive events, and boundary-pushing experiences.
         </h1>
+        <a
+          href="mailto:dickens@dickensagain.com"
+          className="bg-white hover:border-2 border-red-600 rounded-md p-2 text-red-500 text-xs aspect-square h-24 flex flex-col items-center justify-center mt-4"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
+          </svg>
+          <p>get in touch</p>
+        </a>
       </header>
-      <section className="w-full bg-black h-3/4 mt-40 sm:mt-44 md:mt-48 lg:mt-52">
+      <section className="w-full bg-black h-3/4 mt-44 sm:mt-44 md:mt-48 lg:mt-72">
         <div className="px-12 py-12 sm:px-16 md:px-20 lg:px-24">
           <a href="https://www.begrudginglydickens.com" target="_blank">
             <div className="flex flex-col items-center justify-center gap-4 mb-24 w-full bg-red-600 p-8 rounded-lg hover:border-white border-4 border-red-600 max-w-[600px] mx-auto">
